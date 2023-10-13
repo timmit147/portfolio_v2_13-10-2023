3c580bfe938c7caa8ee0a3d342195ae09720b158
timmit147 <timmeeuwsen@hotmail.nl>
2023-10-13 16:15:07 +0200

    portfolio created with nav to other page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var hamburger = document.querySelector('.hamburger');
+
+// Toggle the 'active' class on click
+hamburger.addEventListener('click', function () {
+    this.classList.toggle('active');
+});
