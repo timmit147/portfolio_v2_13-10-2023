38bcb83def9a97fa33b70f40ac37e96437f145ca
timmit147 <timmeeuwsen@hotmail.nl>
2023-10-16 12:50:30 +0200

    menu working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+// Get the hamburger element
 var hamburger = document.querySelector('.hamburger');
+
+// Get the menu element
+var menu = document.querySelector("menu");
+
+// Check if the 'active' class is stored in localStorage and apply it if present
+if (localStorage.getItem('menuActive') === 'true') {
+    menu.classList.add('active');
+}
 
 // Toggle the 'active' class on click
 hamburger.addEventListener('click', function () {
-    this.classList.toggle('active');
+    // Toggle the 'active' class on the menu element
+    menu.classList.toggle('active');
+
+    // Store the current state in localStorage
+    localStorage.setItem('menuActive', menu.classList.contains('active'));
 });
