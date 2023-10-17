a6663a6a6403df54fff4b271e2519b6570ed0ed7
timmit147 <timmeeuwsen@hotmail.nl>
2023-10-17 14:55:16 +0200

    styling fixed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+// Get the hamburger element
 var hamburger = document.querySelector('.hamburger');
 
+// Get the menu element
 var menu = document.querySelector("menu");
 
+// Check if the 'active' class is stored in localStorage and apply it if present
+if (localStorage.getItem('menuActive') === 'true') {
+    menu.classList.add('active');
+}
+
+// Toggle the 'active' class on click
 hamburger.addEventListener('click', function () {
+    // Toggle the 'active' class on the menu element
     menu.classList.toggle('active');
 
+    // Store the current state in localStorage
+    localStorage.setItem('menuActive', menu.classList.contains('active'));
 });
 
 
