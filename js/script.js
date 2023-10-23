b9ee1b0f28d81096330dc2e891366fc59afd9317
timmit147 <timmeeuwsen@hotmail.nl>
2023-10-23 11:56:16 +0200

    change text and remove loading

--- js/script.js
+++ js/script.js
@@ -19,29 +19,29 @@ hamburger.addEventListener('click', function () {
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Delay for 500 milliseconds (adjust the value as needed)
-    setTimeout(function() {
-        document.querySelector("#loading").style.width = "0px";
-    }, 500);
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Delay for 500 milliseconds (adjust the value as needed)
+//     setTimeout(function() {
+//         document.querySelector("#loading").style.width = "0px";
+//     }, 500);
+// });
 
-function handleClick(event) {
-    if (event.target.tagName === 'A' && event.target.hasAttribute('href')) {
-        event.preventDefault();
+// function handleClick(event) {
+//     if (event.target.tagName === 'A' && event.target.hasAttribute('href')) {
+//         event.preventDefault();
 
-        // Disable click event for a short period
-        document.body.style.pointerEvents = 'none';
+//         // Disable click event for a short period
+//         document.body.style.pointerEvents = 'none';
 
-        document.querySelector("#loading").style.width = "100vw";
+//         document.querySelector("#loading").style.width = "100vw";
 
-        setTimeout(function () {
-            window.location.href = event.target.href;
+//         setTimeout(function () {
+//             window.location.href = event.target.href;
 
-            // Enable click event after the redirect
-            document.body.style.pointerEvents = 'auto';
-        }, 500); // Adjust the delay as needed
-    }
-}
+//             // Enable click event after the redirect
+//             document.body.style.pointerEvents = 'auto';
+//         }, 500); // Adjust the delay as needed
+//     }
+// }
 
-document.body.addEventListener("click", handleClick);
+// document.body.addEventListener("click", handleClick);
