b3a5c8c66c1d5c0d8b53fd4e1e75bef16644467b
timmit147 <timmeeuwsen@hotmail.nl>
2023-10-25 12:52:27 +0200

    Title animation added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ hamburger.addEventListener('click', function () {
     menu.classList.toggle('active');
 });
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const titleElement = document.querySelector('#index h1');
+  
+    if (titleElement) {
+      animateTitle(titleElement);
+    }
+  });
+  
+  async function animateTitle(titleElement) {
+    const text = titleElement.textContent;
+    const random = "*&^%$#@?{!/.><=";
+  
+    for (let i = 0; i < text.length; i++) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      titleElement.textContent = titleElement.textContent.replace(titleElement.textContent.charAt(i), random[i]);
+    }
+  
+    await new Promise(resolve => setTimeout(resolve, 100));
+  
+    for (let i = 0; i < text.length; i++) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      titleElement.textContent = titleElement.textContent.replace(titleElement.textContent.charAt(i), text[i]);
+    }
+  }
+  
